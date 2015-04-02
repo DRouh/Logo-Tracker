e15f5b33612fe4efbb8bf3dadb4e6c0a1703153d
Dime <dimerouh@gmail.com>
2015-04-02 19:42:18 +0300

    -Now if tracked object isn't in the frame detector won't crash -Added several feature detectors, matchers to color tracker -Now in every fame ASIFT sampling applies -Processed frames stored as images

--- base color tracker/asiftmatching/asiftmatcher.py
+++ base color tracker/asiftmatching/asiftmatcher.py
@@ -2,36 +2,42 @@
 import numpy as np
 import cv2
 import itertools as it
-from multiprocessing.pool import ThreadPool
 
 # local modules
 from common import Timer
 
 class AsiftMatcher:
-    def __init__(self):
-		# store the keypoint detection method and descriptor method
-         print "Hello from consructor"
+    def __init__(self,matcher):		
+        print "Initializing asift matcher"
+        self.Mactcher = matcher
 
-    def asift_match(self,im1,im2,kp1,desc1,kp2,desc2):
+    def asift_match(self, im1, im2, kp1, desc1, kp2, desc2):
         img1 = im1
         img2 = im2
-        norm = cv2.NORM_L2
-        matcher = cv2.BFMatcher(norm)    
+        norm = cv2.NORM_L2        
         
         def match_and_draw(win):
-            with Timer('matching'):
-                raw_matches = matcher.knnMatch(desc1, trainDescriptors = desc2, k = 2) #2
-            p1, p2, kp_pairs = self.filter_matches(kp1, kp2, raw_matches)
-            if len(p1) >= 4:
-                H, status = cv2.findHomography(p1, p2, cv2.RANSAC, 5.0)
-                print '%d / %d  inliers/matched' % (np.sum(status), len(status))
-                # do not draw outliers (there will be a lot of them)
-                kp_pairs = [kpp for kpp, flag in zip(kp_pairs, status) if flag]
+            noMatches = False
+            kp_pairs = None
+            H = None
+            if(len(desc2) > 1):
+                with Timer('matching'):                
+                    raw_matches = self.Mactcher.knnMatch(desc1, trainDescriptors = desc2, k = 2) #2
+                    
+                p1, p2, kp_pairs = self.filter_matches(kp1, kp2, raw_matches)
+                if len(p1) >= 4 and len(p2) >= 4:
+                    H, status = cv2.findHomography(p1, p2, cv2.RANSAC, 5.0)
+                    print '%d / %d  inliers/matched' % (np.sum(status), len(status))
+                    # do not draw outliers (there will be a lot of them)
+                    kp_pairs = [kpp for kpp, flag in zip(kp_pairs, status) if flag]                    
+                else:
+                    H, status = None, None
+                    noMatches = True
+                    print '%d matches found, not enough for homography estimation' % len(p1)
             else:
-                H, status = None, None
-                print '%d matches found, not enough for homography estimation' % len(p1)
+                noMatches = True
     
-            win, vis = self.explore_match(win, img1, img2, kp_pairs, None, H)
+            win, vis = self.explore_match(win, img1, img2, kp_pairs, noMatches, None, H)
             return win, vis
             
         win, vis = match_and_draw('affine find_obj')
@@ -106,22 +112,29 @@ class AsiftMatcher:
         return keypoints, np.array(descrs)       
         
         
-    def explore_match(self, win, img1, img2, kp_pairs, status = None, H = None):
+    def explore_match(self, win, img1, img2, kp_pairs, noMatches, status = None, H = None):
         h1, w1 = img1.shape[:2]
         h2, w2 = img2.shape[:2]
+        
         vis = np.zeros((max(h1, h2), w1+w2), np.uint8)
         vis[:h1, :w1] = img1
         vis[:h2, w1:w1+w2] = img2
         vis = cv2.cvtColor(vis, cv2.COLOR_GRAY2BGR)
+        
+        if noMatches is True:
+            return win,vis
     
         if H is not None:
             corners = np.float32([[0, 0], [w1, 0], [w1, h1], [0, h1]])
             corners = np.int32( cv2.perspectiveTransform(corners.reshape(1, -1, 2), H).reshape(-1, 2) + (w1, 0) )
             cv2.polylines(vis, [corners], True, (255, 255, 255))
+        else:
+            return win,vis
     
         if status is None:
-            status = np.ones(len(kp_pairs), np.bool_)
-        p1 = np.int32([kpp[0].pt for kpp in kp_pairs])
+            status = np.ones(len(kp_pairs), np.bool_)            
+        p1 = np.int32([kpp[0].pt for kpp in kp_pairs])        
+
         p2 = np.int32([kpp[1].pt for kpp in kp_pairs]) + (w1, 0)
     
         green = (0, 255, 0)
@@ -146,28 +159,6 @@ class AsiftMatcher:
             if inlier:
                 cv2.line(vis, (x1, y1), (x2, y2), green)
         return win, vis 
-        #cv2.imshow(win, vis)
-#        def onmouse(event, x, y, flags, param):
-#            cur_vis = vis
-#            if flags & cv2.EVENT_FLAG_LBUTTON:
-#                cur_vis = vis0.copy()
-#                r = 8
-#                m = (anorm(p1 - (x, y)) < r) | (anorm(p2 - (x, y)) < r)
-#                idxs = np.where(m)[0]
-#                kp1s, kp2s = [], []
-#                for i in idxs:
-#                     (x1, y1), (x2, y2) = p1[i], p2[i]
-#                     col = (red, green)[status[i]]
-#                     cv2.line(cur_vis, (x1, y1), (x2, y2), col)
-#                     kp1, kp2 = kp_pairs[i]
-#                     kp1s.append(kp1)
-#                     kp2s.append(kp2)
-#                cur_vis = cv2.drawKeypoints(cur_vis, kp1s, flags=4, color=kp_color)
-#                cur_vis[:,w1:] = cv2.drawKeypoints(cur_vis[:,w1:], kp2s, flags=4, color=kp_color)
-#    
-#            cv2.imshow(win, cur_vis)
-#        cv2.setMouseCallback(win, onmouse)
-        return win, vis        
         
     def filter_matches(self,kp1, kp2, matches, ratio = 0.75):
         mkp1, mkp2 = [], []
--- base color tracker/main.py
+++ base color tracker/main.py
@@ -1,50 +1,160 @@
-from sklearn.cluster import KMeans
-from PIL import Image
-from pylab import *
-import numpy as np
-import matplotlib.pyplot as plt
-import pickle
-import os
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 import cv2
-import imtools
+import os
+import numpy as np
 import colorsys
+from asiftmatching import asiftmatcher
+from multiprocessing.pool import ThreadPool
 
-path = 'd:/downloads/test/Images for color clustering/'
-imglist = imtools.get_imlist(path)
-clt = KMeans(n_clusters = 3)
-for i in range(len(imglist)):
-    print 'evaluating colors: %d / %d\r' % (i+1, len(imglist)),
-    # load the image and convert it from BGR to RGB so that
-    # we can dispaly it with matplotlib
-    im = cv2.imread(imglist[i]) #cv2.COLOR_BGR2HSV,cv2.COLOR_BGR2RGB
-    im = cv2.cvtColor(im, cv2.COLOR_BGR2RGB)
+FLANN_INDEX_KDTREE = 1  # bug: flann enums are missing
+FLANN_INDEX_LSH    = 6
+class ColourTracker:
     
-    # reshape the image to be a list of pixels
-    im = im.reshape((im.shape[0] * im.shape[1], 3))
+  def __init__(self, name):
+    cv2.namedWindow("ColourTrackerWindow", cv2.CV_WINDOW_AUTOSIZE)
+    self.capture = cv2.VideoCapture(0)    
+    self.scale_down = 4
+    self.Pool = ThreadPool(processes = cv2.getNumberOfCPUs())    
+    chunks = name.split('-')
+    if chunks[0] == 'sift':
+        detector = cv2.SIFT()  #cv2.xfeatures2d.SIFT_create()
+        norm = cv2.NORM_L2
+    elif chunks[0] == 'surf':
+        detector = cv2.SURF(800)
+        norm = cv2.NORM_L2
+    elif chunks[0] == 'orb':
+        detector = cv2.ORB()
+        norm = cv2.NORM_HAMMING
+    elif chunks[0] == 'akaze':
+        detector = cv2.AKAZE_create()
+        norm = cv2.NORM_HAMMING
+    elif chunks[0] == 'brisk':
+        detector = cv2.BRISK_create()
+        norm = cv2.NORM_HAMMING
+    else:
+        return None, None
+    if 'flann' in chunks:
+        if norm == cv2.NORM_L2:
+            flann_params = dict(algorithm = FLANN_INDEX_KDTREE, trees = 5)
+        else:
+            flann_params= dict(algorithm = FLANN_INDEX_LSH,
+                               table_number = 6, # 12
+                               key_size = 12,     # 20
+                               multi_probe_level = 1) #2
+        matcher = cv2.FlannBasedMatcher(flann_params, {})  # bug : need to pass empty dict (#1329)
+    else:
+        matcher = cv2.BFMatcher(norm)
+    self.Detector = detector
+    self.Matcher = matcher
+    print 'using', name
     
-    # cluster the pixel intensities        
-    clt.fit(im)
+  def run(self):    
+    matcher = asiftmatcher.AsiftMatcher(self.Matcher)    
+    #colors = np.array([23,11,111])
+    colors = np.array([170])
+    img1 = cv2.imread("e:\\master thesis\\Logo-Tracker\\base color tracker\\coca-cola.jpg", 0)   
+    kp1,des1 = matcher.affine_detect(self.Detector, img1, mask=None, pool=self.Pool)
+    framenum = 0
     
-    # build a histogram of clusters and then create a figure
-    # representing the number of pixels labeled to each color
-    hist = imtools.centroid_histogram(clt)
-    bar = imtools.plot_colors(hist, clt.cluster_centers_)
-    
-    # show our image
-    plt.figure()
-    plt.title(os.path.basename(imglist[i]))
-    plt.axis("off")
-    plt.imshow(bar)
-    plt.show()
-    #sort by descending    
-    sortedColors = np.array(clt.cluster_centers_[np.argsort(-hist)])
+    while True:
+      f, orig_img = self.capture.read()
+      img_Sift = np.copy(orig_img)
+      
+      #calculate sift and draw it on result_img
+      sift_img = cv2.cvtColor(img_Sift ,cv2.COLOR_BGR2GRAY)       
+      kp,des = matcher.affine_detect(self.Detector, sift_img, mask=None, pool=self.Pool)
+      
+      img = cv2.GaussianBlur(orig_img, (5,5), 0)
+      img = cv2.cvtColor(orig_img, cv2.COLOR_BGR2HSV)
+      img = cv2.resize(img, (len(orig_img[0]) / self.scale_down, len(orig_img) / self.scale_down))
+      
+      boxArray = np.array([self.getBoundingBox(img, col) for col in colors])      
+      
 
-    #save dominant colors in HSV color space
-    hsvColors = np.array([colorsys.rgb_to_hsv(r/255, g/255, b/255) for r, g, b in sortedColors])
+      for i in range(len(boxArray)):
+          if not boxArray[i] == None:              
+              r, g, b = (i * 255 for i in colorsys.hsv_to_rgb(colors[i] / float(179), 1, 1))   
+              filtered_keypoints, filtered_descs = self.filterKeypoints(kp, des, boxArray[i][1][0],
+                                                                        boxArray[i][1][1], 
+                                                                        boxArray[i][3][0], 
+                                                                        boxArray[i][3][1])
+              if filtered_keypoints != None:
+                img_Sift = cv2.drawKeypoints(sift_img, filtered_keypoints, filtered_descs)
+                cv2.drawContours(img_Sift,[boxArray[i]], 0, (b, g, r), 1)   
+                    
+                #do asift matching here              
+                #_, vis = matcher.asift_match(img1, sift_img, kp1, des1, filtered_keypoints, filtered_descs)                           
+                _, vis = matcher.asift_match(sift_img, img1,  filtered_keypoints, filtered_descs,kp1, des1)                           
+                cv2.imshow("Asift Matching", vis)                
+                cv2.imwrite("vis-" + str(framenum) + ".jpg", vis)
+                
+              cv2.drawContours(orig_img,[boxArray[i]], 0, (b, g, r), 1)              
+              
+      #self.Video.write(orig_img)
+      cv2.imshow("ColourTrackerWindow", orig_img)
+      cv2.imshow("SIFT", img_Sift)      
+      
+      framenum += 1
+      if cv2.waitKey(20) == 27:        
+        cv2.destroyWindow("ColourTrackerWindow")
+        cv2.destroyWindow("SIFT")
+        cv2.destroyWindow("Asift Matching")
+
+        self.capture.release()
+        break
     
-    #hsvColors[:,0] *= 359 #multiply first column,e.g. hue by 359. Since percnt to absolute
-    hsvColors[:,0] *= 179
-    hsvColors[:,1] *= 255
-    hsvColors[:,2] *= 255
-    np.savetxt(imglist[i][:-4] + '.txt', hsvColors)
-print    
+  def filterKeypoints(self, kp, des, left_x, left_y, right_x, right_y):
+      """Filters given SIFT-keypoints and return them and desc"""
+      if kp == None:
+          return None,None
+          
+      filtered_keypoints = []
+      filtered_desc = []
+      for i in range(len(kp)):
+          kp_x,kp_y = kp[i].pt
+          if(kp_x >= left_x and kp_x <= right_x and kp_y >= left_y and kp_y <= right_y):
+            filtered_keypoints.append(kp[i])
+            filtered_desc.append(des[i])
+            
+      return filtered_keypoints, np.array(filtered_desc)
+  
+  def getBoundingBox(self, bluredimage, hue):
+      lower = np.array([max(hue - 10, 0), 150, 50])
+      upper = np.array([min(hue + 10, 179), 255, 255])
+      binary = cv2.inRange(bluredimage, lower, upper)
+      
+      #dilate binary image to get wider contours
+      dilation = np.ones((15, 15), "uint8")
+      binary = cv2.dilate(binary, dilation)
+      
+      #While finding contours, you are also finding the hierarchy of the contours.
+      #Hierarchy of the contours is the relation between different contours.
+      #So the flag you used in your code, cv2.RETR_TREE provides all the hierarchical relationship.
+      #cv2.RETR_LIST provides no hierarchy while cv2.RETR_EXTERNAL gives you only external contours.
+      
+      contours, hierarchy = cv2.findContours(binary, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+      max_area = 0
+      largest_contour = None
+      
+      for idx, contour in enumerate(contours):
+        area = cv2.contourArea(contour)
+        if area > max_area:
+          max_area = area
+          largest_contour = contour
+          
+      if not largest_contour == None:         
+        moment = cv2.moments(largest_contour)
+        if moment["m00"] > 1000 / self.scale_down:
+          rect = cv2.minAreaRect(largest_contour)          
+          rect = ((rect[0][0] * self.scale_down, rect[0][1] * self.scale_down), (rect[1][0] * self.scale_down, rect[1][1] * self.scale_down), 0)#rect[2])
+          box = cv2.cv.BoxPoints(rect)
+          box = np.int0(box)
+          return box
+      else:
+          return None
+
+  
+if __name__ == "__main__":
+  colour_tracker = ColourTracker("sift")
+  colour_tracker.run()
