e2ca53dc2fbf7c2f677efccb8f7d100aa52c83cb
Dime <dimerouh@gmail.com>
2015-04-20 21:42:47 +0300

    -fixes

--- toy classificator/fromdump.py
+++ toy classificator/fromdump.py
@@ -13,24 +13,21 @@ from asiftmatching import asiftmatcher
 #Load the images
 all_instance_filenames = []
 all_instance_targets = []
-for f in glob.glob('t/*.asift'):
+for f in glob.glob('asift/*.asift'):
 	target = 1 if 'cocacola' in f else 0
 	all_instance_filenames.append(f)
 	all_instance_targets.append(target)
 print 
-#Convert the images to grayscale, and extract the SURF descriptors
+
 surf_features = []
 counter = 1
-detector, matcher = cv2.SIFT(), cv2.BFMatcher(cv2.NORM_L2)
-asiftMatcher = asiftmatcher.AsiftMatcher(matcher)
-Pool = ThreadPool(processes = 2)#cv2.getNumberOfCPUs())    
 for f in all_instance_filenames:
     print 'Reading image:{0}. {1}/{2}'.format(f, counter, len(all_instance_targets))     
-    surf_features.append(pickle.load(open(f, "rb"))  )   
+    surf_features.append(pickle.load(open(f, "rb")))   
     counter += 1
 
 #Split the images into training and testing data
-train_len = int(len(all_instance_filenames)*.60)
+train_len = int(len(all_instance_filenames)*.80)
 
 indices = np.random.permutation(len(all_instance_filenames))
 training_idx, test_idx = indices[:train_len], indices[train_len:]
@@ -45,12 +42,11 @@ y_test = [all_instance_targets[i]  for i in test_idx]
 #to compute the distances to the centroids for a sample of the instances.
 n_clusters = 300
 print 'Clustering', len(X_train_surf_features), 'features'
-estimator = MiniBatchKMeans(n_clusters=n_clusters, verbose=1)
+estimator = KMeans(n_clusters=n_clusters, verbose=1)
 estimator.fit_transform(X_train_surf_features)
-#with open('KMeansLgo.pkl','wb') as f:
-#    pickle.dump(estimator,f)
+with open('KMeansCoceAndNone.pkl','wb') as f:
+    pickle.dump(estimator,f)
 #estimator = pickle.load(open('estimator.pkl', "rb"))
-
 
 X_train = []
 for instance in surf_features[:train_len]:
@@ -60,7 +56,6 @@ for instance in surf_features[:train_len]:
 		features = np.append(features, np.zeros((1, n_clusters-len(features))))
 	X_train.append(features)
 
-print "len(surf_features[train_len:])",len(surf_features[train_len:])
 X_test = []
 count = 0
 for instance in surf_features[train_len:]:
@@ -81,7 +76,7 @@ print classification_report(y_test, predictions)
 print 'Precision: ', precision_score(y_test, predictions)
 print 'Recall: ', recall_score(y_test, predictions)
 print 'Accuracy: ', accuracy_score(y_test, predictions)
-with open('LogisticRegression.pkl','wb') as f:
+with open('LogisticRegressionCoceAndNone.pkl','wb') as f:
     pickle.dump(clf,f)
     
 print classification_report(y_test, predictions)
