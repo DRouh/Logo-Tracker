1c6b7cf0c95d3ee0523746807473cdf017acff6e
Dime <dimerouh@gmail.com>
2015-04-22 20:00:26 +0300

    -added code for sift bag-of-words classification

---
+++ toy classificator/sift/3. histcalc.py
@@ -0,0 +1,31 @@
+import numpy as np
+from sklearn.cluster import KMeans
+import glob
+import pickle
+
+#Load the images
+all_instance_filenames = []
+for f in glob.glob('sift (all descriptors)/*.sift'):
+	target = 1 if 'cocacola' in f else 0
+	all_instance_filenames.append(f)
+print 
+
+surf_features = []
+counter = 1
+n_clusters = 300    
+estimator = pickle.load(open('KMeansCoceAndNoneSIFT.pkl', "rb"))
+#calc hist for all descsriptors
+for f in all_instance_filenames:
+    print 'Reading image:{0}. {1}/{2}'.format(f, counter, len(all_instance_filenames))     
+    surf_features = pickle.load(open(f, "rb"))   
+    counter += 1
+    train_len = 1
+    clusters = estimator.predict(surf_features)
+    features = np.bincount(clusters)        
+    if len(features) < n_clusters:
+        features = np.append(features, np.zeros((1, n_clusters-len(features))))
+    fname = f + '.hist'     
+    print features
+    with open(fname,'wb') as fi:
+        pickle.dump(features, fi)
+  
