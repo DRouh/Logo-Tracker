6f2a65a1830fe0f36007ba092c130a1b09837074
Dime <dimerouh@gmail.com>
2015-04-25 18:48:39 +0300

    -added sift drawing to sliding window

--- sliding window classificator/main.py
+++ sliding window classificator/main.py
@@ -1,7 +1,28 @@
-# import the necessary packages
+import copy
 from skimage.transform import pyramid_gaussian
+import numpy as np
 import cv2
 import time
+import imutils
+
+
+def pyramid(image, scale=1.5, minSize=(30, 30)):
+    # yield the original image
+    yield image
+
+    # keep looping over the pyramid
+    while True:
+        # compute the new dimensions of the image and resize it
+        w = int(image.shape[1] / scale)
+        image = imutils.resize(image, width=w)
+
+        # if the resized image does not meet the supplied minimum
+        # size, then stop constructing the pyramid
+        if image.shape[0] < minSize[1] or image.shape[1] < minSize[0]:
+            break
+
+        # yield the next image in the pyramid
+        yield image
 
 
 def sliding_window(image, stepSize, windowSize):
@@ -14,11 +35,14 @@ def sliding_window(image, stepSize, windowSize):
 
 if __name__ == '__main__':
     # load the image
-    image = cv2.imread("cocacola.jpg")
+    image = cv2.imread("cocacola.png", cv2.COLOR_BGR2GRAY)
     image = cv2.resize(image, None, fx=0.2, fy=0.2, interpolation=cv2.INTER_CUBIC)
     (winW, winH) = (128, 128)
-
-    for (i, resized) in enumerate(pyramid_gaussian(image, downscale=1.5)):
+    sift = cv2.SIFT()
+    for resized in pyramid(image, scale=1.5):
+        cop = resized.copy()
+        gray = cv2.cvtColor(cop, cv2.COLOR_BGR2GRAY)
+        kp, des = sift.detectAndCompute(gray, None)
         for (x, y, window) in sliding_window(resized, stepSize=32, windowSize=(winW, winH)):
             # if the window does not meet our desired window size, ignore it
             if window.shape[0] != winH or window.shape[1] != winW:
@@ -31,6 +55,8 @@ if __name__ == '__main__':
             # since we do not have a classifier, we'll just draw the window
             clone = resized.copy()
             cv2.rectangle(clone, (x, y), (x + winW, y + winH), (0, 255, 0), 2)
+            clone = cv2.drawKeypoints(clone, kp, flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
             cv2.imshow("Window", clone)
+            cv2.imwrite("clone.png", clone)
             cv2.waitKey(1)
             # time.sleep(0.025)
