b083796b42efd381479f7c2638b939d35cc87f45
Dime <dimerouh@gmail.com>
2015-04-26 18:57:44 +0300

    -Filter keypoints by sliding-window rectangle

--- sliding window classificator/main.py
+++ sliding window classificator/main.py
@@ -1,9 +1,26 @@
-import copy
-from skimage.transform import pyramid_gaussian
-import numpy as np
+import pickle
+from time import sleep
+
 import cv2
-import time
+import numpy as np
+
 import imutils
+
+
+def filterKeypoints(kp, des, left_x, left_y, right_x, right_y):
+    """Filters given SIFT-keypoints by rect and return them and desc"""
+    if kp is None:
+        return None, None
+
+    filtered_keypoints = []
+    filtered_desc = []
+    for i in range(len(kp)):
+        kp_x, kp_y = kp[i].pt
+        if left_x <= kp_x <= right_x and left_y <= kp_y <= right_y:
+            filtered_keypoints.append(kp[i])
+            filtered_desc.append(des[i])
+
+    return filtered_keypoints, np.array(filtered_desc)
 
 
 def pyramid(image, scale=1.5, minSize=(30, 30)):
@@ -34,15 +51,26 @@ def sliding_window(image, stepSize, windowSize):
 
 
 if __name__ == '__main__':
-    # load the image
+
+    # load k-means
+    n_clusters = 300
+    estimator = pickle.load(open('KMeansCoceAndNoneSIFT.pkl', "rb"))
+
     sift = cv2.SIFT()
+
+    # load the image
     image = cv2.imread("cocacola.png", cv2.COLOR_BGR2GRAY)
+
+    # resize image (ex. is too big)
     image = cv2.resize(image, None, fx=0.2, fy=0.2, interpolation=cv2.INTER_CUBIC)
+
     show_keypoints = False
+
+    # size of sliding-window
     (winW, winH) = (128, 128)
 
     for resized in pyramid(image, scale=1.5):
-        #calculate keypoints
+        # calculate keypoints
         cop = resized.copy()
         gray = cv2.cvtColor(cop, cv2.COLOR_BGR2GRAY)
         kp, des = sift.detectAndCompute(gray, None)
@@ -52,6 +80,9 @@ if __name__ == '__main__':
             if window.shape[0] != winH or window.shape[1] != winW:
                 continue
 
+            # take only keypoints that belong to the sliding-window
+            fkp, fds = filterKeypoints(kp, des, x, y, x + winW, y + winH)
+
             # THIS IS WHERE YOU WOULD PROCESS YOUR WINDOW, SUCH AS APPLYING A
             # MACHINE LEARNING CLASSIFIER TO CLASSIFY THE CONTENTS OF THE
             # WINDOW
@@ -60,7 +91,7 @@ if __name__ == '__main__':
             clone = resized.copy()
             cv2.rectangle(clone, (x, y), (x + winW, y + winH), (0, 255, 0), 2)
             if show_keypoints:
-                clone = cv2.drawKeypoints(clone, kp, flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+                clone = cv2.drawKeypoints(clone, fkp, flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
             cv2.imshow("Window", clone)
 
             ch = 0xFF & cv2.waitKey(5)
@@ -69,3 +100,4 @@ if __name__ == '__main__':
                 break
             if ch == ord('s'):
                 show_keypoints = not show_keypoints
+            sleep(0.25)
