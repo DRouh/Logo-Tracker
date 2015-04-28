21ba1a75bbb82fe3ede616af2b9967b0e8b3eb8f
Dime <dimerouh@gmail.com>
2015-04-28 19:27:10 +0300

    Automatically shut the computer down after mining

--- sliding window classificator/RootSift/hardnegativesmining.py
+++ sliding window classificator/RootSift/hardnegativesmining.py
@@ -1,3 +1,5 @@
+from csv import excel
+import os
 from os.path import basename
 import pickle
 from shutil import copyfile, copy
@@ -73,58 +75,55 @@ if __name__ == '__main__':
         all_instance_filenames.append(f)
 
     counter = 0
-    for f in all_instance_filenames:
-        counter += 1
-        print "Computing {0}/{1}".format(counter, len(all_instance_filenames))
-        im = cv2.imread(f)
-        fname = f
-        i = 0
-        found = 0
-        for resized in pyramid(im, scale=1.5):
-            # calculate keypoints
-            cop = resized.copy()
-            gray = cv2.cvtColor(cop, cv2.COLOR_BGR2GRAY)
-            kp = sift.detect(gray)
-            (kps, des) = rs.compute(gray, kp)
-            boxes = []
-            for (x, y, window) in sliding_window(resized, stepSize=32, windowSize=(winW, winH)):
-                i += 1
-                # if the window does not meet our desired window size, ignore it
-                if window.shape[0] != winH or window.shape[1] != winW:
-                    continue
+    try:
+        for f in all_instance_filenames:
+            counter += 1
+            print "Computing {0}/{1}".format(counter, len(all_instance_filenames))
+            im = cv2.imread(f)
+            fname = f
+            i = 0
+            found = 0
+            for resized in pyramid(im, scale=1.5):
+                # calculate keypoints
+                cop = resized.copy()
+                gray = cv2.cvtColor(cop, cv2.COLOR_BGR2GRAY)
+                kp = sift.detect(gray)
+                (kps, des) = rs.compute(gray, kp)
+                boxes = []
+                for (x, y, window) in sliding_window(resized, stepSize=32, windowSize=(winW, winH)):
+                    i += 1
+                    # if the window does not meet our desired window size, ignore it
+                    if window.shape[0] != winH or window.shape[1] != winW:
+                        continue
 
-                # take only keypoints that belong to the sliding-window
-                fkp, fds = filterKeypoints(kp, des, x, y, x + winW, y + winH)
+                    # take only keypoints that belong to the sliding-window
+                    fkp, fds = filterKeypoints(kp, des, x, y, x + winW, y + winH)
 
-                if fds.any():
-                    # calculate b-o-w
-                    clusters = estimator.predict(fds)
-                    features = np.bincount(clusters)
-                    if len(features) < n_clusters:
-                        features = np.append(features, np.zeros((1, n_clusters - len(features))))
-                    prediction = clf.predict(features)
-                else:
-                    prediction = [0]
+                    if fds.any():
+                        # calculate b-o-w
+                        clusters = estimator.predict(fds)
+                        features = np.bincount(clusters)
+                        if len(features) < n_clusters:
+                            features = np.append(features, np.zeros((1, n_clusters - len(features))))
+                        prediction = clf.predict(features)
+                    else:
+                        prediction = [0]
 
-                if prediction == [1]:
-                    found += 1
-                    crop_img = resized.copy()
-                    crop_img = crop_img[x:x + winW, y:y + winH]
-                    name = pathToSaveHard + basename(fname)[:-4] + '_' + str(i) + ".jpg"
-                    cv2.imwrite(name, crop_img)
-                    boxes.append((x, y, x + winW, y + winH))
+                    if prediction == [1]:
+                        found += 1
+                        crop_img = resized.copy()
+                        crop_img = crop_img[x:x + winW, y:y + winH]
+                        name = pathToSaveHard + basename(fname)[:-4] + '_' + str(i) + ".jpg"
+                        cv2.imwrite(name, crop_img)
+                        boxes.append((x, y, x + winW, y + winH))
 
-                # since we do not have a classifier, we'll just draw the window
-                # clone = resized.copy()
-                # cv2.rectangle(clone, (x, y), (x + winW, y + winH), (0, 255, 0), 2)
+                    ch = 0xFF & cv2.waitKey(5)
+                    if ch == 27:
+                        cv2.destroyAllWindows()
+                        break
+            if found == 0:
+                copy(fname, pathToSaveNotFound)
 
-                # if len(boxes) > 0:
-                # for (_, (x0, y0, x1, y1)) in enumerate(boxes):
-                # cv2.rectangle(clone, (x0, y0), (x1, y1), (255, 0, 255), 2)
-                #cv2.imshow("Window", clone)
-                ch = 0xFF & cv2.waitKey(5)
-                if ch == 27:
-                    cv2.destroyAllWindows()
-                    break
-        if found == 0:
-            copy(fname, pathToSaveNotFound)
+        os.system("shutdown /s")
+    except:
+        os.system("shutdown /s")
